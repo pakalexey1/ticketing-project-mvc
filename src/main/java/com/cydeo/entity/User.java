c7e7e77453f6eb6eeb87cb946f37a1e3786949fa
pakalexey <mye604@gmail.com>
2023-10-03 16:54:03 -0400

    Further development

---
+++ src/main/java/com/cydeo/entity/User.java
@@ -0,0 +1,16 @@
+package com.cydeo.entity;
+
+import com.cydeo.enums.Gender;
+
+public class User extends BaseEntity{
+
+    private String firstName;
+    private String lastName;
+    private String userName;
+    private String password;
+    private boolean enabled;
+    private String phone;
+    private Role role;
+    private Gender gender;
+
+}
